3e4e5ccc1830afc2d6fd41af621d82b412eb932d
sakievmi <sakievmi@proton.me>
2026-09-03 22:50:35 +0300

    feat: add function for converting liblistenbrain.Listen to CustomListen

--- modules/music_client.py
+++ modules/music_client.py
@@ -12,9 +12,18 @@ class CustomListen:
     cover_url: str
 
 
+def convert_to_custom_listen(listen: liblistenbrainz.Listen) -> CustomListen:
+    cover_url = settings.DEFAULT_COVER_URL
+    if getattr(listen, "release_group_mbid", None):
+        cover_url = f"https://coverartarchive.org/release-group/{listen.release_group_mbid}/front-500"
+
+    return CustomListen(listen, cover_url)
+
+
 class MusicServiceClient:
-    def __init__(self, username: str):
+    def __init__(self, username: str, history_count: int = 5):
         self.username: str = username
+        self.history_count: int = history_count
 
         # TODO: make listen history
         self.history = []
@@ -24,8 +33,4 @@ class MusicServiceClient:
         if listen is None:
             return None
 
-        cover_url = settings.DEFAULT_COVER_URL
-        if getattr(listen, "release_group_mbid", None):
-            cover_url = f"https://coverartarchive.org/release-group/{listen.release_group_mbid}/front-500"
-
-        return CustomListen(listen, cover_url)
+        return convert_to_custom_listen(listen)
