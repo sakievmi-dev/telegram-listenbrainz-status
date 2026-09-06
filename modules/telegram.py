1e05fd28408ca078b1359bd3a214c3c9672cc8a8
sakievmi <sakievmi@proton.me>
2026-09-06 18:11:55 +0300

    fix: change logger to self.logger

--- modules/telegram.py
+++ modules/telegram.py
@@ -15,7 +15,7 @@ class TelegramClient:
         self.logger = logging.getLogger(__name__)
 
     async def send_initial_message(self, cover_url: str) -> int:
-        logger.info("Sending initial message")
+        self.logger.info("Sending initial message")
 
         try:
             message = await self.bot.send_photo(
@@ -25,14 +25,14 @@ class TelegramClient:
                 chat_id=self.chat_id,
             )
         except Exception as e:
-            logger.error(f"Telegram API returned an error: {e}")
+            self.logger.error(f"Telegram API returned an error: {e}")
 
-        logger.info("The initial message was sent")
+        self.logger.info("The initial message was sent")
 
         return message.message_id
 
     async def update_message(self, message_id: int, text: str, cover_url: str) -> None:
-        logger.info(f"Updating message id:{message_id}")
+        self.logger.info(f"Updating message id:{message_id}")
 
         try:
             await self.bot.edit_message_media(
@@ -45,6 +45,6 @@ class TelegramClient:
                 message_id=message_id,
             )
         except Exception as e:
-            logger.error(f"Telegram API returned an error: {e}")
+            self.logger.error(f"Telegram API returned an error: {e}")
 
-        logger.info(f"Message with id {message_id} was updated.")
+        self.logger.info(f"Message with id {message_id} was updated.")
